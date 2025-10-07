9e77264bcaccf517cdce16d428b2603c3374a6cf
ashrafashraf02 <ashrafbilalmohaidat@gmail.com>
2025-10-07 13:16:41 +0300

    Clusters Website

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -23,15 +23,7 @@ const ContactInfo = () => {
             </div>
             <div>
               <h4 className="font-semibold text-[#3C3C3C]">{t("contact_info.email")}</h4>
-              <a className="text-[#9AA0A6] hover:text-[#00AEEF]" href="mailto:hello@clusters.com">hello@clusters.com</a>
-            </div>
-          </div>
-          
-          <div className="flex items-start">
-            <div className="text-[#00AEEF] mr-4 mt-1">📱</div>
-            <div>
-              <h4 className="font-semibold text-[#3C3C3C]">{t("contact_info.phone")}</h4>
-              <p className="text-[#9AA0A6]">+1 (555) 123-4567</p>
+              <a className="text-[#9AA0A6] hover:text-[#00AEEF]" href="mailto:hello@clusters.com">clusters.jo79@gmail.com</a>
             </div>
           </div>
           
@@ -48,7 +40,7 @@ const ContactInfo = () => {
             </div>
           </div>
           
-          <div className="flex items-start">
+          {/*<div className="flex items-start">
             <div className="text-[#00AEEF] mr-4 mt-1">📍</div>
             <div>
               <h4 className="font-semibold text-[#3C3C3C]">{t("contact_info.address")}</h4>
@@ -58,7 +50,7 @@ const ContactInfo = () => {
                 {isAr ? t("contact_info.address_line3") : "United States"}
               </p>
             </div>
-          </div>
+          </div>*/}
         </div>
       </div>
       
@@ -84,23 +76,16 @@ const ContactInfo = () => {
   <stop offset="1" stopColor="#8C3AAA"/>
         </radialGradient>
         </defs>
-        </svg>, url: "#" },
+        </svg>, url: "https://www.instagram.com/clustersjo/" },
   { name: "LinkedIn", icon: <svg className="rounded-md transition-all duration-300 group-hover:scale-110" xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 72 72" fill="none">
           <path fill-rule="evenodd" clip-rule="evenodd" d="M14.6975 11C12.6561 11 11 12.6057 11 14.5838V57.4474C11 59.4257 12.6563 61.03 14.6975 61.03H57.3325C59.3747 61.03 61.03 59.4255 61.03 57.4468V14.5838C61.03 12.6057 59.3747 11 57.3325 11H14.6975ZM26.2032 30.345V52.8686H18.7167V30.345H26.2032ZM26.6967 23.3793C26.6967 25.5407 25.0717 27.2703 22.4615 27.2703L22.4609 27.2701H22.4124C19.8998 27.2701 18.2754 25.5405 18.2754 23.3791C18.2754 21.1686 19.9489 19.4873 22.5111 19.4873C25.0717 19.4873 26.6478 21.1686 26.6967 23.3793ZM37.833 52.8686H30.3471L30.3469 52.8694C30.3469 52.8694 30.4452 32.4588 30.3475 30.3458H37.8336V33.5339C38.8288 31.9995 40.6098 29.8169 44.5808 29.8169C49.5062 29.8169 53.1991 33.0363 53.1991 39.9543V52.8686H45.7133V40.8204C45.7133 37.7922 44.6293 35.7269 41.921 35.7269C39.8524 35.7269 38.6206 37.1198 38.0796 38.4653C37.8819 38.9455 37.833 39.6195 37.833 40.2918V52.8686Z" fill="#006699"/>
-          </svg>, url: "#" },
-  { name: "Email", icon: <svg className="rounded-md transition-all duration-300 group-hover:scale-110" width="28" height="28" viewBox="0 0 72 72" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M13.0065 56.1236H21.4893V35.5227L9.37109 26.4341V52.4881C9.37109 54.4997 11.001 56.1236 13.0065 56.1236Z" fill="#4285F4"/>
-        <path d="M50.5732 56.1236H59.056C61.0676 56.1236 62.6914 54.4937 62.6914 52.4881V26.4341L50.5732 35.5227" fill="#34A853"/>
-        <path d="M50.5732 19.7693V35.5229L62.6914 26.4343V21.587C62.6914 17.0912 57.5594 14.5282 53.9663 17.2245" fill="#FBBC04"/>
-        <path d="M21.4893 35.5227V19.769L36.0311 30.6754L50.5729 19.769V35.5227L36.0311 46.429" fill="#EA4335"/>
-        <path d="M9.37109 21.587V26.4343L21.4893 35.5229V19.7693L18.0962 17.2245C14.4971 14.5282 9.37109 17.0912 9.37109 21.587Z" fill="#C5221F"/>
-        </svg>   , url: "#" }
-  ].map((social) => (
+          </svg>, url: "#" },].map((social) => (
     <a
       key={social.name}
               href={social.url}
               className="flex items-center justify-center w-12 h-12 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 text-[#3C3C3C] hover:text-[#00AEEF]"
               title={social.name}
+              target="_blank"
             >
               {social.icon}
             </a>
@@ -108,7 +93,7 @@ const ContactInfo = () => {
         </div>
       </div>
       
-      {/* Map Placeholder */}
+      {/* Map Placeholder
         <div className="relative w-full h-[300px] sm:h-[300px] lg:h-[300px]">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d414476.25071221846!2d35.845182585860115!3d31.777339537427405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151b5fb85d7981af%3A0x631c30c0f8dc65e8!2z2LnZhdmR2KfZhg!5e1!3m2!1sar!2sjo!4v1758178110375!5m2!1sar!2sjo"
@@ -117,7 +102,7 @@ const ContactInfo = () => {
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
-       </div>
+       </div>*/}
 
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,17 +32,10 @@ const Footer = () => {
   <stop offset="1" stopColor="#8C3AAA"/>
         </radialGradient>
         </defs>
-        </svg>, url: "#" },
+        </svg>, url: "https://www.instagram.com/clustersjo/" },
   { name: "LinkedIn", icon: <svg className="rounded-md transition-all duration-300 group-hover:scale-110" xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 72 72" fill="none">
           <path fillRule="evenodd" clipRule="evenodd" d="M14.6975 11C12.6561 11 11 12.6057 11 14.5838V57.4474C11 59.4257 12.6563 61.03 14.6975 61.03H57.3325C59.3747 61.03 61.03 59.4255 61.03 57.4468V14.5838C61.03 12.6057 59.3747 11 57.3325 11H14.6975ZM26.2032 30.345V52.8686H18.7167V30.345H26.2032ZM26.6967 23.3793C26.6967 25.5407 25.0717 27.2703 22.4615 27.2703L22.4609 27.2701H22.4124C19.8998 27.2701 18.2754 25.5405 18.2754 23.3791C18.2754 21.1686 19.9489 19.4873 22.5111 19.4873C25.0717 19.4873 26.6478 21.1686 26.6967 23.3793ZM37.833 52.8686H30.3471L30.3469 52.8694C30.3469 52.8694 30.4452 32.4588 30.3475 30.3458H37.8336V33.5339C38.8288 31.9995 40.6098 29.8169 44.5808 29.8169C49.5062 29.8169 53.1991 33.0363 53.1991 39.9543V52.8686H45.7133V40.8204C45.7133 37.7922 44.6293 35.7269 41.921 35.7269C39.8524 35.7269 38.6206 37.1198 38.0796 38.4653C37.8819 38.9455 37.833 39.6195 37.833 40.2918V52.8686Z" fill="#006699"/>
           </svg>, url: "#" },
-  { name: "Email", icon: <svg className="rounded-md transition-all duration-300 group-hover:scale-110" width="28" height="28" viewBox="0 0 72 72" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M13.0065 56.1236H21.4893V35.5227L9.37109 26.4341V52.4881C9.37109 54.4997 11.001 56.1236 13.0065 56.1236Z" fill="#4285F4"/>
-        <path d="M50.5732 56.1236H59.056C61.0676 56.1236 62.6914 54.4937 62.6914 52.4881V26.4341L50.5732 35.5227" fill="#34A853"/>
-        <path d="M50.5732 19.7693V35.5229L62.6914 26.4343V21.587C62.6914 17.0912 57.5594 14.5282 53.9663 17.2245" fill="#FBBC04"/>
-        <path d="M21.4893 35.5227V19.769L36.0311 30.6754L50.5729 19.769V35.5227L36.0311 46.429" fill="#EA4335"/>
-        <path d="M9.37109 21.587V26.4343L21.4893 35.5229V19.7693L18.0962 17.2245C14.4971 14.5282 9.37109 17.0912 9.37109 21.587Z" fill="#C5221F"/>
-        </svg>   , url: "#" }
   ];
 
   return (
@@ -62,6 +55,7 @@ const Footer = () => {
                   key={social.name}
                   href={social.url}
                   title={social.name}
+                  target="_blank"
                 >
                   {social.icon}
                 </a>
@@ -117,10 +111,7 @@ const Footer = () => {
           <div>
             <h4 className="text-lg font-medium mb-4 text-[#00AEEF]">{t("contact")}</h4>
             <ul className="space-y-2 text-gray-300 font-medium">
-              <li className="hover:text-[#00AEEF] cursor-pointer" ><a href="mailto:hello@clusters.com">hello@clusters.com</a></li>
-              <li>+1 (555) 123-4567</li>
-              <li>{isAr ? "123 شارع التكنولوجيا" : "123 Tech Avenue"}</li>
-              <li>{isAr ? "سان فرانسيسكو، كاليفورنيا 94107" : "San Francisco, CA 94107"}</li>
+              <li className="hover:text-[#00AEEF] cursor-pointer" ><a href="mailto:clusters.jo79@gmail.com">clusters.jo79@gmail.com</a></li>
             </ul>
           </div>
         </div>
